937fe8e7c5b529337bfe1a2a0654e4a98ee85500
Piby32 <piby1216@gmail.com>
2025-02-03 14:43:03 +0800

    Create ex1-3.cpp

---
+++ ex1-3.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int a = 10, b = 3;
+    printf("加法: %d + %d = %d\n", a, b, a + b);
+    printf("檢法: %d - %d = %d\n", a, b, a - b);
+    printf("乘法: %d * %d = %d\n", a, b, a * b);
+    printf("除法: %d / %d = %d\n", a, b, a / b);
+    printf("曲除法: %d %% %d = %d\n", a, b, a % b);
+    return 0;
+}
