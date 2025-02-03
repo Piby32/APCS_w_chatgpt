2bdca76c9cbf0dd0ecb19866c822185130e717bb
Piby32 <piby1216@gmail.com>
2025-02-03 15:21:32 +0800

    Create ex1-5.cpp

---
+++ ex1-5.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int numbers[5] = {10, 20, 30, 40, 50}; //初始化陣列
+    
+    printf("第一格元素: %d\n", numbers[0]);
+    printf("第二格元素: %d\n", numbers[2]);
+    
+    return 0;
+}
+    
